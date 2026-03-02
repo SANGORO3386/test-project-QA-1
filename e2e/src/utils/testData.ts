1a9a7e22687d3732948caf399e7cf07f8de03f9d
SANGORO3386 <57318005+SANGORO3386@users.noreply.github.com>
2026-03-02 10:07:54 +0300

    Add files via upload

---
+++ e2e/src/utils/testData.ts
@@ -0,0 +1,15 @@
+export const validUser = {
+  username: 'testuser',
+  password: 'Password123!'
+};
+
+export const invalidUser = {
+  username: '',
+  password: '123'
+};
+
+export const newClient = {
+  firstName: 'Victor',
+  lastName: 'Tester',
+  email: 'victor@test.com'
+};
